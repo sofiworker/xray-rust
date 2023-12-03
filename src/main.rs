66a0d28d486fdedb7efd3704d0d3dba1c97c08f8
sofiworker <sofiworker@outlook.com>
2023-12-03 20:04:15 +0800

    test:parse command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("hello!!");
+    let cmd = clap::Command::new("xray-rust")
+        .bin_name("xray")
+        .subcommand_required(true)
+        .subcommand(
+            clap::command!("config").arg(
+                clap::arg!(--"manifest-path" <PATH>)
+                    .value_parser(clap::value_parser!(std::path::PathBuf)),
+            ),
+        );
+    let matches = cmd.get_matches();
+    let matches = match matches.subcommand() {
+        Some(("example", matches)) => matches,
+        _ => unreachable!("clap should ensure we don't get here"),
+    };
+    let config_path = matches.get_one::<std::path::PathBuf>("config-path");
+    println!("{config_path:?}");
 }
